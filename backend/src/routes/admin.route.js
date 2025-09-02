07c82cc1df9ad250bbe1b797a665fe40e0ab1fca
NuhDemir <drmaa217@gmail.com>
2025-09-02 12:40:36 +0300

    Remove unused checkAdmin import from admin routes
    The checkAdmin import was removed from admin.route.js as it is no longer used in this file.

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -4,7 +4,6 @@ import {
   deleteSong,
   createAlbum,
   deleteAlbum,
-  checkAdmin,
 } from "../controllers/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 import {
